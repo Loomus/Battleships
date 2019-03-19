f61278cc663ab62f01c23f3c19e410568cedbe49
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-03-19 15:33:14 -0600

    Ship init method tested and working

---
+++ test/ship_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_it_has_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+  end
+
+  def test_it_has_health
+    assert_equal 3, @cruiser.health
+  end
+end
+
+
+# pry(main)> cruiser.health
+# #=> 3
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 2
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.health
+# #=> 1
+#
+# pry(main)> cruiser.sunk?
+# #=> false
+#
+# pry(main)> cruiser.hit
+#
+# pry(main)> cruiser.sunk?
+# #=> true
