5f056126660cdf460c97f05826eeea9a50faa161
Andrew Johnson <manilda7@gmail.com>
2019-03-20 14:32:49 -0600

    Created cells hash in board class

---
+++ test/board_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+  end
+
+  def test_board_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_how_keys_are_in_cell_hash
+    assert_equal 16, @board.cells.keys.count
+  end
+end
