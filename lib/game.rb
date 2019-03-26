e206de308ebf8e60262643925d28dd95aedda1c7
Andrew Johnson <manilda7@gmail.com>
2019-03-26 16:54:15 -0600

    Iteration 3 beginnings

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,45 @@
+require_relative './board'
+require_relative './ship'
+require_relative './cell'
+
 class Game
+  def initialize
+    @player_board = Board.new
+    @comp_board = Board.new
+    @sheila = Ship.new("Sheila", 3)
+    @toto = Ship.new("Toto",  2)
+    @cells_taken = []
+  end
 
+  def main_menu
+    p "Welcome to Battleship!"
+    p "Enter p to play! Or q to quit."
+    print "> "
+    answer = gets.chomp
+    if answer == "p"
+      setup_boards
+    else answer == "q"
+      main_menu
+    end
+  end
+
+  def spot_select
+
+  end
+
+  def find_empty_cell
+    coord = @comp_board.cells.find do |key, value|
+      return key if value.empty?
+    end
+  end
+
+  def setup_boards
+    find_empty_cell
+    require 'pry'; binding.pry
+    @cells.keys.each_slice(4).to_a
+    @comp_board.render
+    @comp_board.cells.keys.sample(3) = random_coord
+    until @comp_board.valid_placement? == true
+    "I have laid out my ships on the grid."
+  end
 end
-
-
-
-
-
-
-
-
-p "Welcome to Battleship!"
-p "Enter p to play! Or q to quit."
-print "> "
-answer = gets.chomp
-if answer == p
---
+++ test/player_board_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+require './lib/game'
+
+class Player_boardTest < Minitest::Test
+
+end
